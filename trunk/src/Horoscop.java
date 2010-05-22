4e84f04929faa010ea648d4c6446f25624fe4176
vlad.lep <vlad.lep@deed1ba9-bc1e-e0f0-6f13-9538c181af29>
2010-05-22 15:58:42 +0000

    implementat pt nota 9 - pucntele 2,3 de la Horoscop

--- trunk/src/Horoscop.java
+++ trunk/src/Horoscop.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 
 public class Horoscop 
@@ -12,6 +14,49 @@ public class Horoscop
 	
 	public void loadList()
 	{
-		list = null;
+		list = (ArrayList<Student>) XMLReader.loadList();
+	}
+
+	public void printList() 
+	{
+		System.out.println(list.toString());
+		
+	}
+	public Student findByNr_matr(int nr_matr)
+	{
+		Iterator<Student> it = list.iterator();
+		Student aux;
+		while(it.hasNext())
+		{
+			if( (aux = it.next()).getNr_matr() == nr_matr)
+			{
+				//System.out.println(aux.toString());
+				return aux;
+			}
+			
+		}
+	
+		return null;
+		
+	}
+	
+	public List<Student> findByMedie(double medie)
+	{
+		Iterator<Student> it = list.iterator();
+		Student aux;
+		ArrayList result = new ArrayList<Student>();
+		
+		while(it.hasNext())
+		{
+			if( (aux = it.next()).getMedie() == medie)
+			{
+				//System.out.println(aux.toString());
+				result.add(aux);
+			}
+			
+		}
+	
+		return result;
+		
 	}
 }
